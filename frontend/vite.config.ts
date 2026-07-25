b06cdf75a1143e18454dc62f0eb715cd3502ee53
Rifaya09709 <rifayasafi11@gmail.com>
2026-07-25 23:56:58 +0400

    kkk

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,22 @@
-
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-
   server: {
     port: 5173,
-
     proxy: {
       "/api": {
         target: "https://daraz-website.onrender.com",
         changeOrigin: true,
       },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
